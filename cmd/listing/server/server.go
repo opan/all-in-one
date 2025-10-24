a328acce90fa0e59dadcd94914a54b3b7c0852d8
opan <opan.neutron@gmail.com>
2025-10-24 17:38:56 +0700

    in progress changes
    Signed-off-by: opan <opan.neutron@gmail.com>

--- cmd/listing/server/server.go
+++ cmd/listing/server/server.go
@@ -2,10 +2,11 @@ package server
 
 import (
 	"github.com/all-in-one/internal/config"
+	"github.com/all-in-one/internal/storage"
 	"github.com/rs/zerolog"
 )
 
-type listingServer struct {
+type server struct {
 	config config.Config
 	log    zerolog.Logger
 }
@@ -15,14 +16,19 @@ type Opts struct {
 	Logger zerolog.Logger
 }
 
-func New(opts Opts) *listingServer {
-	return &listingServer{
+func New(opts Opts) *server {
+	return &server{
 		config: opts.Config,
 		log:    opts.Logger,
 	}
 }
 
-func (s *listingServer) Start() error {
+func (s *server) Start() error {
 	// Implementation of server start logic
+
+	s.log.Info().Msg("Initiating server start...")
+
+	storage := storage.NewStorage(s.config, s.log)
+
 	return nil
 }
