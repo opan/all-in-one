ad86a3dd035ae087a0a52e9b1603870f6fb0cd01
opan <opan.neutron@gmail.com>
2025-12-24 15:37:21 +0700

    fix: auth http middleware
    Signed-off-by: opan <opan.neutron@gmail.com>

--- internal/http/auth.go
+++ internal/http/auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/all-in-one/internal/logging"
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
@@ -24,15 +25,19 @@ type UserClaims struct {
 func (h *HTTP) JWTAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		log := logging.GetLoggerFromContext(ctx)
 
 		if userClaims, ok := h.tryDirectAuth(ctx, r); ok {
 			ctx = context.WithValue(ctx, UserContextKey, userClaims)
+
+			log.Info().Str("username", userClaims.Username).Msg("direct authentication bypass used")
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 
 		userClaims, err := h.validateJWT(ctx, r)
 		if err != nil {
+			log.Error().Err(err).Str("username", userClaims.Username).Msg("unauthorized access")
 			SendUnauthorized(w, err.Error())
 			return
 		}
@@ -43,19 +48,17 @@ func (h *HTTP) JWTAuth(next http.Handler) http.Handler {
 }
 
 func (h *HTTP) tryDirectAuth(ctx context.Context, r *http.Request) (UserClaims, bool) {
+	log := logging.GetLoggerFromContext(ctx)
+
 	if !h.config.Auth.DirectAuthEnabled {
 		return UserClaims{}, false
 	}
 
 	username := r.Header.Get("x-direct-auth-username")
 	if username == "" {
+		log.Warn().Msg("empty username in the header for direct auth method")
 		return UserClaims{}, false
 	}
-
-	h.log.Warn().
-		Ctx(ctx).
-		Str("username", username).
-		Msg("direct authentication bypass used")
 
 	return UserClaims{
 		SessionID: "direct-auth",
@@ -64,32 +67,35 @@ func (h *HTTP) tryDirectAuth(ctx context.Context, r *http.Request) (UserClaims, 
 }
 
 func (h *HTTP) validateJWT(ctx context.Context, r *http.Request) (UserClaims, error) {
+	log := logging.GetLoggerFromContext(ctx)
+
 	cookie, err := r.Cookie("access_token")
 	if err != nil {
-		h.log.Error().Ctx(ctx).Msg("cannot find JWT token")
+		log.Error().Msg("cannot find JWT token")
 		return UserClaims{}, fmt.Errorf("Unauthorized: missing or invalid token: %v", err)
 	}
 
 	token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			log.Error().Err(jwt.ErrSignatureInvalid).Msg("invalid JWT signature")
 			return nil, jwt.ErrSignatureInvalid
 		}
 		return []byte(h.config.Auth.JWTSecret), nil
 	})
 
 	if err != nil {
-		h.log.Error().Ctx(ctx).Msg("failed to parse JWT token")
+		log.Error().Err(err).Msg("failed to parse JWT token")
 		return UserClaims{}, fmt.Errorf("Unauthorized: invalid token: %v", err)
 	}
 
 	if !token.Valid {
-		h.log.Error().Ctx(ctx).Msg("invalid JWT token")
+		log.Error().Err(err).Msg("invalid JWT token")
 		return UserClaims{}, fmt.Errorf("Unauthorized: invalid token")
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		h.log.Error().Ctx(ctx).Msg("invalid JWT claims")
+		log.Error().Msg("invalid JWT claims")
 		return UserClaims{}, fmt.Errorf("Unauthorized: invalid token claims")
 	}
 
