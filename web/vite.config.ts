428d97dd08511704a067a07c76fd70d28729c67d
opan <opan.neutron@gmail.com>
2026-05-16 12:21:47 +0700

    fix: include /r path in vite
    Signed-off-by: opan <opan.neutron@gmail.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
 				target: "http://localhost:8080",
 				changeOrigin: true,
 			},
+			"/r": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+			},
 		},
 		watch: {
 			usePolling: true,
